c27a71949527f05eaad7c8cf94974e8a614f2904
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2023-10-14 14:59:26 +0200

    minor changes

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,10 @@
 const inputRef = document.querySelector("#name-input");
-const span = document.querySelector("#name-output");
+const spanRef = document.querySelector("#name-output");
 
 const defaultValue = span.textContent;
 
 inputRef.addEventListener("input", (event) => {
   const inputValue = event.target.value;
 
-  span.textContent = inputValue || defaultValue;
+  spanRef.textContent = inputValue || defaultValue;
 });
